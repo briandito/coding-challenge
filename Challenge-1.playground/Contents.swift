003ebabcbcc1318cf7aa9ab8354aa921a3603502
Briandito <briandito.p@gmail.com>
2018-07-29 00:52:29 +0800

    thought about another way to solve the problem, but havent finished.

--- Challenge-1.playground/Contents.swift
+++ Challenge-1.playground/Contents.swift
@@ -3,34 +3,21 @@
 func SumDigit(start: Int, end: Int) -> Int {
     
     var index = start
-    var evenNumbers = [Int]
+    var evenNumbers = [Int]()
+    var sum: Int = 0
     
     //loop through the numbers between start and end
     while index <= end{
-    
-        var lastDigit: Int
-        var firstDigits: [Int]
-        var sum: Int
         
-        //check if number is bigger than 10
-        if index <= 10{
-            evenNumbers.append(index % 10)
+
+        while index/10 > 0 {
+            sum = sum + index%10
         }
-        else{
-            //check and save the last digit
-            lastDigit = index % 10
-            
-            //check and save the first till last-1 digit.
-            
-            //go to next number
-        }
-        
-        index += 1
         
     }
     
-    return end
+    return 0
     
 }
 
-SumDigit(start: 20, end: 44)
+SumDigit(start: 2, end: 7)
